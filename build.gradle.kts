3378be62e215decaf9f99ff556a06cc02fc4352a
nikhom-pu <nikhom072@gmail.com>
2025-07-30 15:52:42 +0700

    Fix: Add repositories to root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,18 @@
 plugins {
     id("com.android.application") version "8.4.0"
-    kotlin("android") version "1.9.23"
+    kotlin("android") version "1.9.0"
 }
 
-android {
-    namespace = "com.example.gpstracker"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.gpstracker"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
