ddb39695834fa4fa37a9fd5333b34382e1a9c280
nikhom-pu <nikhom072@gmail.com>
2025-07-30 15:44:57 +0700

    Fix: Update build.gradle.kts and settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.4.0"
+    kotlin("android") version "1.9.23"
 }
 
 android {
@@ -9,37 +9,21 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gpstracker"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.android.gms:play-services-location:21.1.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
